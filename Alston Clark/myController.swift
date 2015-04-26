3182c011dbbc4a47fe89466d0a11db4a773f9262
Acespace <Alston@Alstons-MacBook-Air.local>
2015-04-26 18:45:02 -0400

    wwdc15 app
    Signed-off-by: Acespace <Alston@Alstons-MacBook-Air.local>

---
+++ Alston Clark/myController.swift
@@ -0,0 +1,13 @@
+//
+//  myController.swift
+//  Alston Clark
+//
+//  Created by Alston Clark on 4/26/15.
+//  Copyright (c) 2015 Alston Clark. All rights reserved.
+//
+
+import UIKit
+
+class myController: UIViewController {
+
+}
